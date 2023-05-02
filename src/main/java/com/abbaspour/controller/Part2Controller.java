d735232651f4e01c67e93fbe1168c0ee8d5496a9
Mehdi Abbaspoor <abbaspoor.mehdi@gmail.com>
2023-05-01 21:17:43 -0500

    fix styling and remove unused libraries

--- src/main/java/com/abbaspour/controller/Part2Controller.java
+++ src/main/java/com/abbaspour/controller/Part2Controller.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
-import java.util.BitSet;
 
 @RestController
 @CrossOrigin(maxAge = 3600)
