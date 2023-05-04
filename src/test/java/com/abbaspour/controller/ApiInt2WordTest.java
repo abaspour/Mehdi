1d26e3ae300f6b7999eca9e6e5d718ce4bd2a415
Mehdi Abbaspoor <abbaspoor.mehdi@gmail.com>
2023-05-03 22:50:32 -0500

    Update ApiInt2WordTest.java

--- src/test/java/com/abbaspour/controller/ApiInt2WordTest.java
+++ src/test/java/com/abbaspour/controller/ApiInt2WordTest.java
@@ -7,9 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -24,6 +27,8 @@ public class ApiInt2WordTest {
     @MockBean
     private DecodeService decodeService;
     @MockBean
+    private Int2WordController int2WordController;
+    @MockBean
     private IntegerToWordService integerToWordService;
 
     @Test
@@ -35,14 +40,21 @@ public class ApiInt2WordTest {
         String output = "Twelve";
 
         given(integerToWordService.toEnglishWordConverter(number)).willReturn(output);
+        MvcResult mvcResult = mockMvc.perform(get("/api/int-to-word/{language}/{number}", language,number))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String responseBody = response.getContentAsString();
+        assertThat(responseBody).isEqualTo("Expected response string");
 
         // when -  action or the behaviour that we are going test
-        ResultActions response = mockMvc.perform(get("/api/int-to-word/{language}/{number}", language,number));
-        // to check the code is ok just uncomment next line
-        // bitMap.setMachine_on(true);
-        // then - verify the output
-        response.andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(jsonPath("$.machine_on", is(output)));
+//        ResultActions response = mockMvc.perform(get("/api/int-to-word/{language}/{number}", language,number));
+//        // to check the code is ok just uncomment next line
+//        // bitMap.setMachine_on(true);
+//        // then - verify the output
+//        response.andExpect(status().isOk())
+//                .andDo(print())
+//                .andExpect(jsonPath("$.machine_on", is(output)));
     }
 }
